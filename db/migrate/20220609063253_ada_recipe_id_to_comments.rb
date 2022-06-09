b7d16f9ac6f83d59513a18c20ff85f2348456a7e
MuzamilHussainHaideri <haiderimuzamil@gmail.com>
2022-06-09 11:45:24 +0500

    comments added

---
+++ db/migrate/20220609063253_ada_recipe_id_to_comments.rb
@@ -0,0 +1,4 @@
+class AdaRecipeIdToComments < ActiveRecord::Migration[6.0]
+  def change
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_09_054621) do
+ActiveRecord::Schema.define(version: 2022_06_09_063253) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.text "comment"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "recipes", force: :cascade do |t|
     t.string "title"
